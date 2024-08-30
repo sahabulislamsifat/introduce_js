e15868eb8e29d049a69ae3bae17fe012fd06f8d0
sahabulislamsifat <sahabulislamsifat@gmail.com>
2024-08-30 17:09:06 +0600

    practice

---
+++ task1.js
@@ -0,0 +1,9 @@
+var taka = 1000;
+
+var orangePrice = 300;
+var applePrice = 400;
+
+var total = 700;
+
+console.log(orangePrice + applePrice);
+console.log(taka - total);
---
+++ task2.js
@@ -0,0 +1,29 @@
+// const input = "75.25, 65, 80, 35.45, 99.50";
+
+// function calculateAverage(marks) {
+//     marks.split(75.25, 65, 80, 35.45, 99.5).map(5);
+
+//     const sum = marksArray.reduce((acc, marks) => acc + marks, 0);
+
+//     const average = sum;
+
+//     return average.tofixed(2);
+// }
+
+// console.log(average);
+
+
+var subject = "mathematics, physics, chemistry, biology & English";
+
+var marks = (75.25 + 65 + 80 + 35.45 + 99.5);
+
+var total = parseFloat(355.2);
+
+var numberOfSubject = 5;
+var average = (total / 5);
+
+
+// var average = (marks / 5);
+// var average = (marks / 5 .toFixed(2));
+
+console.log(average.toFixed(2));
